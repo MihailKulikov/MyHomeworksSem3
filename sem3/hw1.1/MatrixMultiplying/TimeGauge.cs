87268c94bb18f9afb18e1cac50f79fd1d248db25
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2020-09-05 00:37:36 +0300

    Add XML documentation for TimeGauge.cs

--- sem3/hw1.1/MatrixMultiplying/TimeGauge.cs
+++ sem3/hw1.1/MatrixMultiplying/TimeGauge.cs
@@ -3,8 +3,20 @@ using System.Diagnostics;
 
 namespace MatrixMultiplying
 {
+    /// <summary>
+    /// Represents time gauge. Provides methods to calculate elapsed time of specified func.
+    /// </summary>
     public static class TimeGauge
     {
+        /// <summary>
+        /// Calculate elapsed time of specified func.
+        /// </summary>
+        /// <param name="measurableFunc">The specified func.</param>
+        /// <param name="arg">Func argument for invocation.</param>
+        /// <param name="result">The result of the func invocation.</param>
+        /// <typeparam name="TArg">The type of func parameter.</typeparam>
+        /// <typeparam name="TRes">The type of the return value of the func.</typeparam>
+        /// <returns></returns>
         public static TimeSpan CalculateElapsedTime<TArg, TRes>(Func<TArg, TRes> measurableFunc, TArg arg, out TRes result)
         {
             var stopwatch = new Stopwatch();
