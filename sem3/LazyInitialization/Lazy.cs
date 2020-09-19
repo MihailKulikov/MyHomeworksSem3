9c21e2c6e6c69c2a0e6c01eb9ac3f2d3fa65ddac
Mikhail <kulikovmihail90@gmail.com>
2020-09-19 21:57:53 +0300

    Added not thread safe ILazy implementation

---
+++ sem3/LazyInitialization/Lazy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LazyInitialization
+{
+    public class Lazy<T> : ILazy<T>
+    {
+        private bool isValueCreated;
+        private readonly Func<T> supplier;
+        private T value;
+
+        public Lazy(Func<T> supplier)
+        {
+            this.supplier = supplier;
+        }
+        
+        public T Get()
+        {
+            if (isValueCreated) return value;
+            value = supplier.Invoke();
+            isValueCreated = true;
+
+            return value;
+        }
+    }
+}
