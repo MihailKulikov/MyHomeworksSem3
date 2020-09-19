2116ce722656d2fc316e989086fd0eb062a03fa2
Mikhail <kulikovmihail90@gmail.com>
2020-09-20 00:25:20 +0300

    Added thread safe lazy

---
+++ sem3/LazyInitialization/ThreadSafeLazy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace LazyInitialization
+{
+    public class ThreadSafeLazy<T> : ILazy<T>
+    {
+        private volatile bool isValueCreated;
+        private readonly Mutex mutex = new Mutex();
+        private readonly Func<T> supplier;
+        private T value;
+
+        public ThreadSafeLazy(Func<T> supplier)
+        {
+            this.supplier = supplier;
+        }
+        
+        public T Get()
+        {
+            if (isValueCreated) return value;
+            mutex.WaitOne();
+            if (!isValueCreated)
+            {
+                value = supplier.Invoke();
+                isValueCreated = true;
+            }
+            mutex.ReleaseMutex();
+
+            return value;
+        }
+    }
+}
