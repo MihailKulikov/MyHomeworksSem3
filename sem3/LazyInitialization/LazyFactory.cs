ce51c63daff56e09b76a7cb40e202ec4188cab3f
Mikhail <kulikovmihail90@gmail.com>
2020-09-20 00:25:32 +0300

    Added lazy factory

---
+++ sem3/LazyInitialization/LazyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LazyInitialization
+{
+    public static class LazyFactory
+    {
+        public static ILazy<T> CreateThreadSafeLazy<T>(Func<T> supplier)
+            => new ThreadSafeLazy<T>(supplier);
+
+        public static ILazy<T> CreateLazy<T>(Func<T> supplier)
+            => new Lazy<T>(supplier);
+    }
+}
