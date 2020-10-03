ef2f824b1db7a9701227ee736521e4570aae6b30
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2020-10-03 14:52:29 +0300

    XML documentation was added.

--- sem3/hw3.1/ThreadPoolRealisation/IMyTask.cs
+++ sem3/hw3.1/ThreadPoolRealisation/IMyTask.cs
@@ -2,10 +2,28 @@
 
 namespace ThreadPoolRealisation
 {
+    /// <summary>
+    /// Represents an operation that executes in the <see cref="MyThreadPool"/>.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result produced by the <see cref="IMyTask{TResult}"/></typeparam>
     public interface IMyTask<out TResult>
     {
+        /// <summary>
+        /// Gets a value that indicates whether the task has completed.
+        /// </summary>
         bool IsCompleted { get; }
+        
+        /// <summary>
+        /// Gets the result value of this <see cref="IMyTask{TResult}"/>.
+        /// </summary>
         TResult Result { get; }
-        IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> continueWithFunc);
+        
+        /// <summary>
+        /// Creates a continuation that executes in the <see cref="MyThreadPool"/> when the target <see cref="IMyTask{TResult}"/> completes.
+        /// </summary>
+        /// <param name="continuationFunction">A function to run when the <see cref="IMyTask{TResult}"/> completes. When run, the delegate will be passed the completed task as an argument.</param>
+        /// <typeparam name="TNewResult">The type of the result produced by the continuation.</typeparam>
+        /// <returns>A new continuation <see cref="IMyTask{TResult}"/>.</returns>
+        IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> continuationFunction);
     }
 }
--- sem3/hw3.1/ThreadPoolRealisation/MyThreadPool.cs
+++ sem3/hw3.1/ThreadPoolRealisation/MyThreadPool.cs
@@ -5,6 +5,9 @@ using System.Threading;
 
 namespace ThreadPoolRealisation
 {
+    /// <summary>
+    /// Provides a pool of threads that can be used to execute tasks.
+    /// </summary>
     public class MyThreadPool
     {
         private class MyTask<TResult> : IMyTask<TResult>
@@ -39,14 +42,14 @@ namespace ThreadPoolRealisation
                 private set => result = value;
             }
 
-            public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> continueWithFunc)
+            public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> continuationFunction)
             {
-                if (continueWithFunc == null)
+                if (continuationFunction == null)
                 {
-                    throw new ArgumentNullException(nameof(continueWithFunc));
+                    throw new ArgumentNullException(nameof(continuationFunction));
                 }
 
-                var task = new MyTask<TNewResult>(() => continueWithFunc(Result), threadPool);
+                var task = new MyTask<TNewResult>(() => continuationFunction(Result), threadPool);
                 if (IsCompleted)
                 {
                     threadPool.Submit(task);
@@ -117,14 +120,19 @@ namespace ThreadPoolRealisation
         private readonly CountdownEvent countdownEventForShutdown;
         private readonly BlockingCollection<Action> collectionOfPendingTasks = new BlockingCollection<Action>();
 
-        public MyThreadPool(int threadsCount)
+        /// <summary>
+        /// Initialize a <see cref="MyThreadPool"/> instance with the specified number of threads.
+        /// </summary>
+        /// <param name="numberOfThreads">Specified number of threads.</param>
+        /// <exception cref="ArgumentException"><paramref name="numberOfThreads"/> is not positive.</exception>
+        public MyThreadPool(int numberOfThreads)
         {
-            if (threadsCount <= 0)
+            if (numberOfThreads <= 0)
             {
                 throw new ArgumentException("Threads' count is not positive.");
             }
 
-            var threads = new Thread[threadsCount];
+            var threads = new Thread[numberOfThreads];
             countdownEventForShutdown = new CountdownEvent(threads.Length);
             for (var i = 0; i < threads.Length; i++)
             {
@@ -133,6 +141,14 @@ namespace ThreadPoolRealisation
             }
         }
 
+        /// <summary>
+        /// Creates and starts a task of type TResult for the specified function delegate.
+        /// </summary>
+        /// <param name="func">A function delegate that returns the future result to be available through the task.</param>
+        /// <typeparam name="TResult">The type of the result available through the task.</typeparam>
+        /// <returns>The started task.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="func"/> is null.</exception>
+        /// <exception cref="InvalidOperationException"><see cref="MyThreadPool"/> shutdowned.</exception>
         public IMyTask<TResult> Submit<TResult>(Func<TResult> func)
         {
             if (func == null)
@@ -156,6 +172,10 @@ namespace ThreadPoolRealisation
             collectionOfPendingTasks.Add(task.Run);
         }
 
+        /// <summary>
+        /// Terminate threads from the <see cref="MyThreadPool"/>.
+        /// Already running tasks are not interrupted, but new tasks are not accepted for execution by threads from the pool.
+        /// </summary>
         public void Shutdown()
         {
             if (cancellationTokenSource.IsCancellationRequested) return;
