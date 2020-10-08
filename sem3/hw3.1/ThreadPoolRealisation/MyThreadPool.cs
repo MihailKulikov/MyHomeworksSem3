8adfec317c39b7132eaaa727722834b423900207
MihailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2020-10-08 14:42:01 +0300

    Hw3.1 (#3)
    * Created a solution
    
    * Solution  renamed
    
    * Plugs added
    
    * Threads' count should be positive
    
    * Submitted func should not be null
    
    * Submit method implemented
    
    * Result property should throw Aggregate exception if func throws exception
    
    * ContinueWith realisation added
    
    * Should throw ArgumentNullException when calling ContinueWith with null argument.
    
    * Should Throw AggregateException When Calling ContinueWith Of Task That Throws Exception
    
    * Should Throw MyThreadPoolShutdownedException When Trying To Submit To Shutdowned MyThreadPool
    
    * Some tests added
    
    * TODO added
    
    * appveyor.yml updated
    
    * Tests that checks thread safety added
    
    * Tried to implement shutdown(not working)
    
    * SynchronizedQueue was added to take out the synchronization logic
    
    * Made small fixes
    
    * All works except shutdown
    
    * All works (maybe shutdown works wrong)
    
    * Final version
    
    * XML documentation was added.
    
    * Implementation of IDispose for MyThreadPool was added
    
    * Disposing MyThreadPool was added to tests.
    
    * Style fix
    
    * ContinueWith should throw exception when thread pool shutdowned.
    
    * Some fixes were made
    
    * Catching OperationCanceledException was fixed
    
    * Possible deadlock in ContinueWith method was fixed

---
+++ sem3/hw3.1/ThreadPoolRealisation/MyThreadPool.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ThreadPoolRealisation
+{
+    /// <summary>
+    /// Provides a pool of threads that can be used to execute tasks.
+    /// </summary>
+    public sealed class MyThreadPool : IDisposable
+    {
+        private class MyTask<TResult> : IMyTask<TResult>
+        {
+            private Func<TResult> func;
+            private readonly ManualResetEvent manualResetEvent = new ManualResetEvent(false);
+            private TResult result;
+            private AggregateException aggregateException;
+            private readonly Queue<Action> queueOfContinueWithTasks = new Queue<Action>();
+            private readonly MyThreadPool threadPool;
+            private volatile bool isCompleted;
+
+            public MyTask(Func<TResult> func, MyThreadPool threadPool)
+            {
+                this.threadPool = threadPool;
+                this.func = func;
+            }
+
+            public bool IsCompleted
+            {
+                get => isCompleted;
+                private set => isCompleted = value;
+            }
+
+            public TResult Result
+            {
+                get
+                {
+                    manualResetEvent.WaitOne();
+                    return GetResultOrThrowException();
+                }
+                private set => result = value;
+            }
+
+            public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> continuationFunction)
+            {
+                if (continuationFunction == null)
+                {
+                    throw new ArgumentNullException(nameof(continuationFunction));
+                }
+
+                if (threadPool.cancellationTokenSource.IsCancellationRequested)
+                {
+                    throw new InvalidOperationException("Thread pool shutdowned.");
+                }
+
+                TNewResult Func() => continuationFunction(Result);
+                if (IsCompleted)
+                {
+                    return threadPool.Submit(Func);
+                }
+
+                lock (queueOfContinueWithTasks)
+                {
+                    if (IsCompleted)
+                    {
+                        return threadPool.Submit(Func);
+                    }
+
+                    var task = new MyTask<TNewResult>(Func, threadPool);
+                    queueOfContinueWithTasks.Enqueue(() => threadPool.Submit(task));
+                    return task;
+                }
+            }
+
+            public void Run()
+            {
+                try
+                {
+                    Result = func();
+                }
+                catch (Exception e)
+                {
+                    aggregateException = new AggregateException(e);
+                }
+                finally
+                {
+                    lock (queueOfContinueWithTasks)
+                    {
+                        while (queueOfContinueWithTasks.Count != 0)
+                        {
+                            queueOfContinueWithTasks.Dequeue().Invoke();
+                        }    
+
+                        IsCompleted = true;
+                    }
+
+                    manualResetEvent.Set();
+                    func = null;
+                }
+            }
+
+            private TResult GetResultOrThrowException()
+            {
+                if (aggregateException != null)
+                {
+                    throw aggregateException;
+                }
+
+                return result;
+            }
+
+            public void Dispose()
+            {
+                manualResetEvent.Dispose();
+                threadPool.Dispose();
+            }
+        }
+
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        private readonly CountdownEvent countdownEventForShutdown;
+        private readonly BlockingCollection<Action> collectionOfPendingTasks = new BlockingCollection<Action>();
+
+        /// <summary>
+        /// Initialize a <see cref="MyThreadPool"/> instance with the specified number of threads.
+        /// </summary>
+        /// <param name="numberOfThreads">Specified number of threads.</param>
+        /// <exception cref="ArgumentException"><paramref name="numberOfThreads"/> is not positive.</exception>
+        public MyThreadPool(int numberOfThreads)
+        {
+            if (numberOfThreads <= 0)
+            {
+                throw new ArgumentException("Threads' count is not positive.");
+            }
+
+            var threads = new Thread[numberOfThreads];
+            countdownEventForShutdown = new CountdownEvent(threads.Length);
+            for (var i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(() => ExecuteTasks(cancellationTokenSource.Token));
+                threads[i].Start();
+            }
+        }
+
+        /// <summary>
+        /// Creates and starts a task of type TResult for the specified function delegate.
+        /// </summary>
+        /// <param name="func">A function delegate that returns the future result to be available through the task.</param>
+        /// <typeparam name="TResult">The type of the result available through the task.</typeparam>
+        /// <returns>The started task.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="func"/> is null.</exception>
+        /// <exception cref="InvalidOperationException"><see cref="MyThreadPool"/> shutdowned.</exception>
+        public IMyTask<TResult> Submit<TResult>(Func<TResult> func)
+        {
+            if (func == null)
+            {
+                throw new ArgumentNullException(nameof(func));
+            }
+
+            var task = new MyTask<TResult>(func, this);
+            try
+            {
+                collectionOfPendingTasks.Add(task.Run, cancellationTokenSource.Token);
+            }
+            catch (OperationCanceledException)
+            {
+                throw new InvalidOperationException("Thread pool shutdowned.");
+            }
+
+            return task;
+        }
+
+        private void Submit<TResult>(MyTask<TResult> task)
+        {
+            collectionOfPendingTasks.Add(task.Run);
+        }
+
+        /// <summary>
+        /// Terminate threads from the <see cref="MyThreadPool"/>.
+        /// Already running tasks are not interrupted, but new tasks are not accepted for execution by threads from the pool.
+        /// </summary>
+        public void Shutdown()
+        {
+            if (cancellationTokenSource.IsCancellationRequested) return;
+            cancellationTokenSource.Cancel();
+            countdownEventForShutdown.Wait();
+        }
+
+        private void ExecuteTasks(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    if (collectionOfPendingTasks.TryTake(out var taskRunAction))
+                    {
+                        taskRunAction.Invoke();
+                    }
+                    else
+                    {
+                        countdownEventForShutdown.Signal();
+                        break;
+                    }
+                }
+                else
+                {
+                    Action taskRunAction = null;
+                    try
+                    {
+                        taskRunAction = collectionOfPendingTasks.Take(cancellationToken);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                    }
+
+                    taskRunAction?.Invoke();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Release all resources used by the current instance of the <see cref="MyThreadPool"/> class.
+        /// </summary>
+        public void Dispose()
+        {
+            Shutdown();
+            cancellationTokenSource.Dispose();
+            countdownEventForShutdown.Dispose();
+            collectionOfPendingTasks.Dispose();
+        }
+    }
+}
