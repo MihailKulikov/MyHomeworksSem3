0bca50b7ad8ec743259ef997572a54c2762ad2c3
MikhailKulikov <61084995+MihailKulikov@users.noreply.github.com>
2020-11-10 15:41:53 +0300

    First test for assembly handler was added

---
+++ sem3/MyNUnit/RunnerTests/AssemblyHandlerShould.cs
@@ -0,0 +1,23 @@
+﻿using MyNUnit.Runner;
+using MyNUnit.Runner.Interfaces;
+using NUnit.Framework;
+
+namespace RunnerTests
+{
+    public class AssemblyHandlerShould
+    {
+        private IAssemblyHandler assemblyHandler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            assemblyHandler = new AssemblyHandler();
+        }
+        
+        [Test]
+        public void Return_Nothing_When_There_Are_No_Unloaded_Assemblies_In_Directory()
+        {
+            Assert.That(assemblyHandler.GetTestClassesFromAssemblies("."), Is.Empty);
+        }
+    }
+}
