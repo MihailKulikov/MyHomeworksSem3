559dadc6acdf4c1e547d8e96aecc05dd9b4cacfe
Mikhail <kulikovmihail90@gmail.com>
2021-01-04 02:25:16 +0300

    Added form for uploading assemblies

---
+++ sem3/MyNUnit/MyNUnitWeb/Pages/Assemblies/Upload.cshtml.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using MyNUnitWeb.Data;
+using MyNUnitWeb.Utilities;
+
+namespace MyNUnitWeb.Pages.Assemblies
+{
+    public class LoadModel : PageModel
+    {
+        private readonly MyNUnitWebContext context;
+        private readonly long fileSizeLimit;
+        private readonly string[] permittedExtensions;
+        private readonly string targetFilePath;
+
+        [BindProperty]
+        public FileUpload FileUpload { get; set; }
+
+        public string Result { get; private set; }
+
+        public List<string> SavedFileNames => Directory.EnumerateFiles(targetFilePath)
+            .Select(fileName => fileName.Split('\\')[^1])
+            .ToList();
+
+        public LoadModel(MyNUnitWebContext context, IConfiguration config)
+        {
+            fileSizeLimit = config.GetValue<long>("FileSizeLimit");
+            targetFilePath = config.GetValue<string>("StoredFilesPath");
+            permittedExtensions = config.GetValue<string[]>("PermittedExtensions");
+            this.context = context;
+            permittedExtensions = new[] { ".dll" };
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostUploadAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                Result = "Please correct the form";
+
+                return Page();
+            }
+
+            foreach (var formFile in FileUpload.FormFiles)
+            {
+                var formFileContent = await FileHelpers.ProcessFormFile<LoadModel>(
+                    formFile, ModelState, permittedExtensions, fileSizeLimit);
+                if (!ModelState.IsValid)
+                {
+                    Result = "File extension should be .dll";
+
+                    return Page();
+                }
+
+                var filePath = Path.Combine(targetFilePath, formFile.FileName);
+                await using var fileStream = System.IO.File.Create(filePath);
+                await fileStream.WriteAsync(formFileContent);
+                SavedFileNames.Add(filePath);
+            }
+
+            return Page();
+            //_context.Assemblies.Add(Assembly);
+            //await _context.SaveChangesAsync();
+
+            //return RedirectToPage("./Index");
+        }
+    }
+
+    public class FileUpload
+    {
+        [Required]
+        public IFormFileCollection FormFiles { get; set; }
+    }
+}
---
+++ sem3/MyNUnit/MyNUnitWeb/Utilities/FileHelpers.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace MyNUnitWeb.Utilities
+{
+    public static class FileHelpers
+    {
+        public static async Task<byte[]> ProcessFormFile<T>(IFormFile formFile,
+            ModelStateDictionary modelState, string[] permittedExtensions,
+            long sizeLimit)
+        {
+            var fieldDisplayName = string.Empty;
+
+            // Use reflection to obtain the display name for the model
+            // property associated with this IFormFile. If a display
+            // name isn't found, error messages simply won't show
+            // a display name.
+            MemberInfo property =
+                typeof(T).GetProperty(
+                    formFile.Name.Substring(formFile.Name.IndexOf(".",
+                    StringComparison.Ordinal) + 1));
+
+            if (property != null)
+            {
+                if (property.GetCustomAttribute(typeof(DisplayAttribute)) is
+                    DisplayAttribute displayAttribute)
+                {
+                    fieldDisplayName = $"{displayAttribute.Name} ";
+                }
+            }
+
+            // Don't trust the file name sent by the client. To display
+            // the file name, HTML-encode the value.
+            var trustedFileNameForDisplay = WebUtility.HtmlEncode(
+                formFile.FileName);
+
+            // Check the file length. This check doesn't catch files that only have 
+            // a BOM as their content.
+            if (formFile.Length == 0)
+            {
+                modelState.AddModelError(formFile.Name,
+                    $"{fieldDisplayName}({trustedFileNameForDisplay}) is empty.");
+
+                return Array.Empty<byte>();
+            }
+
+            if (formFile.Length > sizeLimit)
+            {
+                var megabyteSizeLimit = sizeLimit / 1048576;
+                modelState.AddModelError(formFile.Name,
+                    $"{fieldDisplayName}({trustedFileNameForDisplay}) exceeds " +
+                    $"{megabyteSizeLimit:N1} MB.");
+
+                return new byte[0];
+            }
+
+            try
+            {
+                using var memoryStream = new MemoryStream();
+                await formFile.CopyToAsync(memoryStream);
+
+                // Check the content length in case the file's only
+                // content was a BOM and the content is actually
+                // empty after removing the BOM.
+                if (memoryStream.Length == 0)
+                {
+                    modelState.AddModelError(formFile.Name,
+                        $"{fieldDisplayName}({trustedFileNameForDisplay}) is empty.");
+                }
+
+                if (!IsValidFileExtension(
+                    formFile.FileName, memoryStream, permittedExtensions))
+                {
+                    modelState.AddModelError(formFile.Name,
+                        $"{fieldDisplayName}({trustedFileNameForDisplay}) file " +
+                        "type isn't permitted or the file's signature " +
+                        "doesn't match the file's extension.");
+                }
+                else
+                {
+                    return memoryStream.ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                modelState.AddModelError(formFile.Name,
+                    $"{fieldDisplayName}({trustedFileNameForDisplay}) upload failed. " +
+                    $"Please contact the Help Desk for support. Error: {ex.HResult}");
+                // Log the exception
+            }
+
+            return new byte[0];
+        }
+
+        private static bool IsValidFileExtension(string fileName, Stream data, string[] permittedExtensions)
+        {
+            if (string.IsNullOrEmpty(fileName) || data == null || data.Length == 0)
+            {
+                return false;
+            }
+
+            var ext = Path.GetExtension(fileName).ToLowerInvariant();
+
+            if (string.IsNullOrEmpty(ext) || !permittedExtensions.Contains(ext))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
