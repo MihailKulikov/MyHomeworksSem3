d58ab7a0c65ac2d1e76a7ec0ec44aa4133370a05
Mikhail <kulikovmihail90@gmail.com>
2021-02-15 19:35:02 +0300

    Trying to connect

--- sem3/hw4.1/ClientGUI/ModelView.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ClientGUI
-{
-    class ModelView
-    {
-    }
-}
---
+++ sem3/hw4.1/ClientGUI/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Client;
+using Client.Interfaces;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ClientGUI
+{
+    class ViewModel
+    {
+        public int Port { get; set; } = 1;
+        public string Address { get; set; } = "localhost";
+
+        private IFtpClient ftpClient;
+
+        public ViewModel()
+        {
+        }
+
+        public async void Connect()
+        {
+            ftpClient = new FtpClient(
+                new FtpClientStreamHandler(await Task.Run(() => new TcpClient(Address, Port).GetStream())));
+        }
+    }
+}
